8c06b7549fc578d851ffc8a4a99320efb861d6ab
shikharbahikmagar <shikharbahik5@gmail.com>
2024-03-22 20:51:57 +0545

    navbar change for mobile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,6 +14,7 @@ function Navbar() {
                 className='md:ml-[210px] ml-[50px] text-2xl text-slate-300 font-semibold font-serif'>
                 Sb.
             </motion.div>
+
             <motion.ul
                 initial={{ y: -150 }}
                 animate={{ y: 0 }}
@@ -33,7 +34,7 @@ function Navbar() {
                 animate={{ y: 0 }}
                 transition={{ delay: .2, type: 'spring', stiffness: 50 }}
                 className='ml-[50%] md:hidden block'>
-                <button className='relative group bg-transparent'>
+                {/* <button className='relative group bg-transparent'>
                     <div className="relative flex overflow-hidden items-center justify-center rounded-full w-[50px] h-[50px] transform transition-all bg-purple-700 ring-0 ring-gray-300 hover:ring-8 group-focus:ring-4 ring-opacity-30 duration-200 shadow-md">
                         <div className="flex flex-col justify-between w-[20px] h-[20px] transform transition-all duration-300 origin-center overflow-hidden">
                             <div className="bg-white h-[2px] w-7 transform transition-all duration-300 origin-left group-focus:rotate-[42deg]"></div>
@@ -41,7 +42,9 @@ function Navbar() {
                             <div className="bg-white h-[2px] w-7 transform transition-all duration-300 origin-left group-focus:-rotate-[42deg]"></div>
                         </div>
                     </div>
-                </button>
+                </button> */}
+                <Link to="/livestream"> <h2 className='md:hidden text-slate-300 text-xl relative after:bg-purple-400 after:absolute after:h-[2px] after:w-0 after:bottom-0 after:left-0 hover:after:w-full hover:text-purple-400 after:transition-all after:duration-[.7s] cursor-pointer'>Watch IPL</h2></Link>
+
             </motion.div>
         </nav>
 
